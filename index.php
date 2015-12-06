0863557bcc55f7ad736818ffe805023150dbf9e6
Your Name <swills@beyond-print.com>
2015-12-06 18:55:50 -0500

    testing local dev environment

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-echo 'this is my initial test php file for opsworks testing.';
-echo '<hr />';
-echo 'and now i will edit the php in a git clone on my laptop, commit it and deploy it.';
-include_once('random.php');
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+// Set the script name
+define('TL_SCRIPT', 'index.php');
+
+// Initialize the system
+define('TL_MODE', 'FE');
+require __DIR__ . '/system/initialize.php';
+
+// Run the controller
+$controller = new FrontendIndex;
+$controller->run();
