b09406dc6c41f288859d3ed37768310e73135ae5
Your Name <swills@beyond-print.com>
2015-12-06 06:23:52 -0500

    testing local dev environment

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo 'this is my initial test php file for opsworks testing.';
 echo '<hr />';
 echo 'and now i will edit the php in a git clone on my laptop, commit it and deploy it.';
-
+include_once('random.php');
